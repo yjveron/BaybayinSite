ed551b3c38504e70ef84c2ab2c20461f14a7dcd8
megtuason <megtuason@gmail.com>
2019-03-26 01:26:47 +0800

    update3

---
+++ update1/js/script.js
@@ -0,0 +1,50 @@
+var circle1 = document.getElementById("circle-1");
+var circle2 = document.getElementById("circle-2");
+var circle3 = document.getElementById("circle-3");
+var circle4 = document.getElementById("circle-4");
+
+var line1 = document.getElementById("line-1");
+var line2 = document.getElementById("line-2");
+var line3 = document.getElementById("line-3");
+
+function intro() {
+  circle1.className = "circle circle-active";
+  circle2.className = "circle circle-future";
+  circle3.className = "circle circle-future";
+  circle4.className = "circle circle-future";
+  line1.className = "line line-inactive";
+  line2.className = "line line-inactive";
+  line3.className = "line line-inactive";
+}
+
+function vowels() {
+  circle1.className = "circle circle-past";
+  circle2.className = "circle circle-active";
+  circle3.className = "circle circle-future";
+  circle4.className = "circle circle-future";
+  line1.className = "line line-active";
+  line2.className = "line line-inactive";
+  line3.className = "line line-inactive";
+}
+
+function consonants() {
+  circle1.className = "circle circle-past";
+  circle2.className = "circle circle-past";
+  circle3.className = "circle circle-active";
+  circle4.className = "circle circle-future";
+  line1.className = "line line-active";
+  line2.className = "line line-active";
+  line3.className = "line line-inactive";
+}
+
+function rules() {
+  circle1.className = "circle circle-past";
+  circle2.className = "circle circle-past";
+  circle3.className = "circle circle-past";
+  circle4.className = "circle circle-active";
+  line1.className = "line line-active";
+  line2.className = "line line-active";
+  line3.className = "line line-active";
+}
+
+
