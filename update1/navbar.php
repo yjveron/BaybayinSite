cd9627ced5c0ea6eeb87243eeedcd21e82d1c613
yjveron <yjveron@gmail.com>
2019-03-31 22:35:53 +0800

    updates

---
+++ update1/navbar.php
@@ -0,0 +1,9 @@
+<div class = "navbar">
+	<ul>
+	    <li><a href="../index.html" id="home-navbar">HOME</a></li>
+	    <li><a href="../script.html" id="script-navbar">SCRIPT</a></li>
+	    <li><a href="precolonial.html" id="discover-nav">DISCOVER</a></li>
+	    <li><a href="../resources.html" id="resources-nav">RESOURCES</a></li>
+    </ul>
+	<h3>BAYBAYIN NATIN</h3>
+</div>
