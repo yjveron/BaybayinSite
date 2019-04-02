c47420538b82ba6f04d3a52d9c3417191c91887f
yjveron <yjveron@gmail.com>
2019-04-02 13:53:27 +0800

    Functional horizontal timeline

--- update1/js/discover.js
+++ update1/js/discover.js
@@ -7,14 +7,26 @@ var line1 = document.getElementById("line-1");
 var line2 = document.getElementById("line-2");
 var line3 = document.getElementById("line-3");
 
-var colonial1 = document.getElementById("colonial-1");
+var precol1 = document.getElementById("precolonial-1");
+var precol2 = document.getElementById("precolonial-2");
+var col1 = document.getElementById("colonial-1");
+var col2 = document.getElementById("colonial-2");
+var art1 = document.getElementById("artifacts-1");
+var art2 = document.getElementById("artifacts-2");
+var theo1 = document.getElementById("theories-1");
+var theo2 = document.getElementById("theories-2");
+
+var parts = [precol1, precol2, col1, col2, art1, art2, theo1, theo2]; 
+
 
 // function ifVisible(partID) {
 //   var isVisible = partID.style.display == "flex";
 //   return true;
 // }
 
-function precolonial() {
+// https://stackoverflow.com/questions/195951/how-to-change-an-elements-class-with-javascript
+
+function precolonial1() {
   circle1.className = "circle circle-active";
   circle2.className = "circle circle-future";
   circle3.className = "circle circle-future";
@@ -22,9 +34,16 @@ function precolonial() {
   line1.className = "line line-inactive";
   line2.className = "line line-inactive";
   line3.className = "line line-inactive";
+
+  for (i = 0; i < parts.length ; i++){
+    parts[i].className = "hidden";
+    if (i === 0) {
+      parts[i].className = "disc col";
+    }
+  }
 }
 
-function colonial() {
+function colonial1() {
   circle1.className = "circle circle-past";
   circle2.className = "circle circle-active";
   circle3.className = "circle circle-future";
@@ -32,9 +51,16 @@ function colonial() {
   line1.className = "line line-active";
   line2.className = "line line-inactive";
   line3.className = "line line-inactive";
+
+  for (i = 0; i < parts.length ; i++){
+    parts[i].className = "hidden";
+    if (i === 2) {
+      parts[i].className = "disc col";
+    }
+  }
 }
 
-function artifacts() {
+function artifacts1() {
   circle1.className = "circle circle-past";
   circle2.className = "circle circle-past";
   circle3.className = "circle circle-active";
@@ -42,9 +68,16 @@ function artifacts() {
   line1.className = "line line-active";
   line2.className = "line line-active";
   line3.className = "line line-inactive";
+
+  for (i = 0; i < parts.length ; i++){
+    parts[i].className = "hidden";
+    if (i === 4) {
+      parts[i].className = "disc col";
+    }
+  }
 }
 
-function theories() {
+function theories1() {
   circle1.className = "circle circle-past";
   circle2.className = "circle circle-past";
   circle3.className = "circle circle-past";
@@ -52,6 +85,22 @@ function theories() {
   line1.className = "line line-active";
   line2.className = "line line-active";
   line3.className = "line line-active";
+
+  for (i = 0; i < parts.length ; i++){
+    parts[i].className = "hidden";
+    if (i === 6) {
+      parts[i].className = "disc th-1";
+    }
+  }
 }
 
+function checkVisible() {
+  if (styleCheck === "flex") {
+      return false; 
+      
+  }
+}
 
+function right(){
+
+}
